9f595c47acf40975022adb1cb42722fab103400e
Alicia <23alicia.piyi@glimnet.se>
2024-10-17 11:27:11 +0300

    Hej

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ app.get('*', (req, res, next) => {
     counter++;
 
     // number of requests...
-    console.log('counter', counter);
+    console.log('counter hejhej', counter);
 
     next();
 });
