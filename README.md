06a25e2a1ac24475f49bf2a8fde2769e7a082707
Alicia <23alicia.piyi@glimnet.se>
2024-10-17 11:24:13 +0300

    changed folder

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# NODEJS-FULLSTACK
+hejhej
---
+++ backend/server.js
@@ -0,0 +1,34 @@
+// dependencies
+import express from 'express';
+
+// variables
+const PORT = 80;
+let counter = 0;
+
+// express environment using name 'app'
+const app = express();
+
+// middleware - do something - continue
+app.get('*', (req, res, next) => {
+
+    // increment...
+    counter++;
+
+    // number of requests...
+    console.log('counter', counter);
+
+    next();
+});
+
+
+// routes - listen on requests
+app.get('/', (req, res) => {
+
+    res.send("Hello world...");
+});
+
+
+// start server...
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
